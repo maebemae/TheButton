48583d5055d868ae442d534d048a7c525c34f9f5
Mae Nova <maize.does.github@mail.maebe.me>
2024-12-23 16:36:24 +0800

    Apply gitignore

--- The Buttoner/Debug/gbmUSBdevice/USBdev/Src/usb_hw_f1.d
+++
@@ -1,2 +0,0 @@
-gbmUSBdevice/USBdev/Src/usb_hw_f1.o: \
- /Users/mae/STM32CubeIDE/workspace_1.17.0/gbmUSBdevice/USBdev/Src/usb_hw_f1.c
--- The Buttoner/Debug/gbmUSBdevice/USBdev/Src/usb_hw_l4.d
+++
@@ -1,2 +0,0 @@
-gbmUSBdevice/USBdev/Src/usb_hw_l4.o: \
- /Users/mae/STM32CubeIDE/workspace_1.17.0/gbmUSBdevice/USBdev/Src/usb_hw_l4.c
